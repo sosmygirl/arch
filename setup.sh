91f1d185504eb249c2f3aaddf7305b72c514d1c9
sosmygirl <332152361li@gmail.com>
2019-12-29 08:56:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,43 +12,43 @@ print_title() {
 	echo ""
 }
 arch_chroot() {
-	arch-chroot /mnt /bin/bash -c "${1}"
+	arch-chroot /mnt/usb /bin/bash -c "${1}"
 }
 
 #替换仓库列表
 update_mirrorlist(){
 	print_title "update_mirrorlist"
-	tmpfile=$(mktemp --suffix=-mirrorlist)	
-	url="https://www.archlinux.org/mirrorlist/?country=CN&protocol=http&protocol=https&ip_version=4"
-	curl -so ${tmpfile} ${url} 
-	sed -i 's/^#Server/Server/g' ${tmpfile}
-	mv -f ${tmpfile} /etc/pacman.d/mirrorlist;
+	echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist;
         #pacman -Syy --noconfirm
+}
+#擦除移动硬盘
+factory_partitions(){
+        dd if=/dev/zero of=/dev/sdc bs=logical-sector-size seek=0 count=sectors status=progress
 }
 #开始分区
 create_partitions(){
 	print_title "create_partitions"
-	parted -s /dev/sda mklabel msdos
-	parted -s /dev/sda mkpart primary ext4 1M 525M
-	parted -s /dev/sda mkpart primary linux-swap 525M 4800M
-	parted -s /dev/sda mkpart primary ext4 4800M 100%
-	parted -s /dev/sda set 1 boot on
-	parted -s /dev/sda print
+	parted -s /dev/sdc mklabel msdos
+	parted -s /dev/sdc mkpart primary ext4 1M 525M
+	parted -s /dev/sdc mkpart primary linux-swap 525M 4800M
+	parted -s /dev/sdc mkpart primary ext4 4800M 100%
+	parted -s /dev/sdc set 1 boot on
+	parted -s /dev/sdc print
 }
 #开始格式化
 format_partitions(){
 	print_title "format_partitions"
-	mkfs.vfat -F32 /dev/sda1 
-	mkswap /dev/sda2 
-	mkfs.ext4 /dev/sda3 
+	mkfs.vfat -F32 /dev/sdc1 
+	mkswap /dev/sdc2 
+	mkfs.ext4 /dev/sdc3 
 }
 #挂载分区
 mount_partitions(){
 	print_title "mount_partitions"
-	mount /dev/sda3 /mnt
-	swapon /dev/sda2
-        mkdir /mnt/boot
-	mount /dev/sda1 /mnt/boot
+	mount /dev/sdac3 /mnt/usb
+	swapon /dev/sdc2
+        mkdir /mnt/usb/boot
+	mount /dev/sdc1 /mnt/usb/boot
 	lsblk
 }
 #最小安装
@@ -60,7 +60,7 @@ install_baseSystem(){
 #生成标卷文件表
 generate_fstab(){
 	print_title "generate_fstab"
-	genfstab -U /mnt >> /mnt/etc/fstab
+	genfstab -U /mnt/usb >> /mnt/usb/etc/fstab
 }
 
 #配置系统时间,地区和语言
@@ -69,10 +69,10 @@ configure_system(){
 	arch_chroot "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
 	arch_chroot "hwclock --systohc --utc"
 	arch_chroot "mkinitcpio -p linux"
-	echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-	echo "zh_CN.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+	echo "en_US.UTF-8 UTF-8" >> /mnt/usb/etc/locale.gen
+	echo "zh_CN.UTF-8 UTF-8" >> /mnt/usb/etc/locale.gen
 	arch_chroot "locale-gen"
-	echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+	echo "LANG=en_US.UTF-8" > /mnt/usb/etc/locale.conf
 }
 
 #安装驱动程序
@@ -88,6 +88,7 @@ configrue_drive(){
 #安装网络管理程序
 configrue_networkmanager(){
        print_title "configrue_networkmanager"
+       arch_chroot "ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules"
        arch_chroot "pacman -S --noconfirm iw wireless_tools wpa_supplicant dialog netctl networkmanager networkmanager-openconnect rp-pppoe network-manager-applet net-tools -y"
        arch_chroot "systemctl enable NetworkManager.service"      
 }
@@ -96,8 +97,8 @@ configrue_networkmanager(){
 configrue_bootloader(){
        print_title "configrue_bootloader"
        arch_chroot "pacman -S --noconfirm grub -y"
-       arch_chroot "grub-install --target=i386-pc /dev/sda"
-       #arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=boot" (efi引导)
+       arch_chroot "grub-install --target=i386-pc --boot-directory /boot /dev/sdc"
+       arch_chroot "grub-install --target=x86_64-efi --efi-directory /boot --boot-directory /boot --removable"
        arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg" 
 }
 
@@ -106,11 +107,11 @@ configrue_bootloader(){
 configure_hostname(){
 	print_title "configure_hostname"
 	read -p "Hostname [ex: archlinux]: " host_name
-	echo "$host_name" > /mnt/etc/hostname
-	if [[ ! -f /mnt/etc/hosts.aui ]]; then
-	cp /mnt/etc/hosts /mnt/etc/hosts.aui
+	echo "$host_name" > /mnt/usb/etc/hostname
+	if [[ ! -f /mnt/usb/etc/hosts.aui ]]; then
+	cp /mnt/usb/etc/hosts /mnt/usb/etc/hosts.aui
 	else
-	cp /mnt/etc/hosts.aui /mnt/etc/hosts
+	cp /mnt/usb/etc/hosts.aui /mnt/usb/etc/hosts
 	fi
 	arch_chroot "sed -i '/127.0.0.1/s/$/ '${host_name}'/' /etc/hosts"
 	arch_chroot "sed -i '/::1/s/$/ '${host_name}'/' /etc/hosts"
@@ -133,7 +134,9 @@ configure_username(){
 
 
 
+
 update_mirrorlist
+factory_partitions
 create_partitions
 format_partitions
 mount_partitions
